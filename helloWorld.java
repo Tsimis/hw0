78db926954b4c7ad923313e755f023d72e0f332e
Tsimis <157445349+Tsimis@users.noreply.github.com>
2024-01-26 16:15:57 -0500

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,15 @@
+// Turner Simis, homework 0 for 225
+//#1) I have read the syallabus.
+//#2) In the course shecduale, I noticed that we are going to be looking at big O complexity 
+// which was something I found pretty confusing when we briefly look at it in 125 so I am 
+// nervous but also excited to revisted that again.
+//#3) The only way I can think of to describe objects in computer science is they are like
+// variables but with more variables to describe whatever you want. Objects are very versitile 
+// and be implemented in many different ways within a program.
+//#4)
+class HelloWorld{
+    public int HelloWorld1(){
+        System.out.println("Hello World!");
+        return 2;
+}
+}
